dc9a7663ec77086d6274bbb809127a551277372a
zzzsyyy <me@zzzsy.top>
2022-04-12 16:35:23 +0800

    update

--- C/C-td/3.2.2.c
+++ C/C-td/3.2.2.c
@@ -5,42 +5,36 @@
 
 float get_rand_num_by_MB() {
   static int Q = 0;
-  static double ret;
+  double ret;
   // Marsaglia-Bray 算法
-  if (Q) {
-    Q = 0;
-    return ret;
-  } else {
-    double v1, v2, s;
-    do {
-      v1 = (rand() / ((double)RAND_MAX)) * 2.0 - 1.0;
-      v2 = (rand() / ((double)RAND_MAX)) * 2.0 - 1.0;
-      s = v1 * v1 + v2 * v2;
-    } while (s >= 1.0 || s == 0.0);
-    s = sqrt(-2.0 * log(s) / s);
-    ret = v1 * s;
-    Q = 1;
-    return v2 * s;
-  }
-  return ret;
+  double v1, v2, s;
+  do {
+    v1 = (rand() / ((double)RAND_MAX)) * 2.0 - 1.0;
+    v2 = (rand() / ((double)RAND_MAX)) * 2.0 - 1.0;
+    s = v1 * v1 + v2 * v2;
+  } while (s >= 1.0 || s == 0.0);
+  s = sqrt(-2.0 * log(s) / s);
+  Q = 1-Q;
+  return Q?v1 * s:v2*s;
 }
 
 int main() {
   int n = 1000, max = 0, m = 1;
   double arr[] = {2.0,  1.8,  1.6,  1.4,  1.2,  1.0,  0.8,
-                    0.6,  0.4,  0.2,  0.0,  -0.2, -0.4, -0.6,
-                    -0.8, -1.0, -1.2, -1.4, -1.6, -1.8, -2.0};
+                  0.6,  0.4,  0.2,  0.0,  -0.2, -0.4, -0.6,
+                  -0.8, -1.0, -1.2, -1.4, -1.6, -1.8, -2.0};
   srand((unsigned)time(NULL)); //随机数初始化
-  int l = sizeof(arr)/sizeof(arr[0]);
-  int times[l+1];
-  for (int i=0;i<l+1;i++)times[i]=0;
+  int l = sizeof(arr) / sizeof(arr[0]);
+  int times[l + 1];
+  for (int i = 0; i < l + 1; i++)
+    times[i] = 0;
   for (int i = 0; i < n; i++) {
     double a = get_rand_num_by_MB();
     for (int j = 0; j < l; j++) {
       if (a > arr[j]) {
         times[l - j]++;
         break;
-      } else if (a <= arr[l-1]) {
+      } else if (a <= arr[l - 1]) {
         times[0]++;
         break;
       }
--- C/C-td/td_3/3.1.c
+++ C/C-td/td_3/3.1.c
@@ -5,23 +5,25 @@
 int main() {
   int n, max=0, m = 1;
   int times[26] = {0};
-  time_t t;
-  srand((unsigned)time(&t));//随机数初始化
+  srand((unsigned)time(NULL));//随机数初始化
 
-  n = 1000;
+  n = 10000;
   for (int i = 0; i < n; i++) {
-    int ch = 'a' + rand() % 26;
+    char ch = 'a' + rand() % 26;
     times[ch - 'a']++;
   }
   //获取出现最多的次数 && 位数
   for (int i=0;i<26;i++) {max=(times[i] > max)? times[i] : max;}
+  printf("%d",max);
   while (max/10>0){
-      max/=10;
+      max=max/10;
       m *=10; 
   }
+  printf("m=%d\n",m);
   //打印统计数据
   for (int i=0;i<26;i++){
     int num = times[i]/m;
+    printf("%d",times[i]);
     printf("the current %c show: ", i+'a');
     for (int j=0;j<num;j++){
         printf("*");
