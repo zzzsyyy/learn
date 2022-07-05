48a589d0ad3e2758279e64dac808f118b3de6d90
zzzsyyy <me@zzzsy.top>
2022-07-06 01:19:50 +0800

    update

---
+++ C/100.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 10000
+
+int run();
+
+int main() {
+  int count = 0;
+  srand((unsigned)time(NULL));
+  for (int i = 0; i < N; i++) {
+    count += run();
+  }
+  printf("%lf", 1 - (double)count / N);
+}
+
+int run() {
+  int arr[100] = {0};
+  int num;
+  int len = 0;
+  int len_max = 0;
+  for (int i = 0; i < 100; i++) {
+    if (arr[i] != 0) {
+      continue;
+    } else {
+      arr[i] = 1;
+      len = 0;
+      num = rand() % 100;
+      if (num == i) {
+        len = 1;
+        continue;
+      }
+      while (num != i) {
+        while (arr[num] == 1 && num != i) {
+          num = rand() % 100;
+        }
+        arr[num] = 1;
+        len++;
+      }
+      len_max = (len_max > len) ? len_max : len;
+    }
+  }
+  return len_max > 50;
+}
